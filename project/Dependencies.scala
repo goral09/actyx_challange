3c4ef4da95854440053d03289621c5e88bfddcff
Mateusz Gorski <mateusz.gorski@hellosoda.com>
2016-09-06 10:25:56 +0200

    Refactor project dependencies file.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,37 +2,39 @@ import sbt._
 
 object Dependencies {
   lazy val circeDependencies = Seq(
-                                    "io.circe" %% "circe-core",
-                                    "io.circe" %% "circe-generic",
-                                    "io.circe" %% "circe-parser"
-                                  ).map(_ % "0.4.1")
+		"io.circe" %% "circe-core",
+    "io.circe" %% "circe-generic",
+    "io.circe" %% "circe-parser"
+    ).map(_ % VersionOf.circe)
 
   lazy val monixDependencies = Seq(
-                                    "io.monix" %% "monix" % "2.0-RC10"
-                                  )
+    "io.monix" %% "monix" % VersionOf.monix)
 
-  lazy val akkaDependencies = Seq(
-                                   "com.typesafe.akka" %% "akka-http-experimental" % "2.4.9-RC2"
-                                 )
+  lazy val `akka-http` = Seq(
+    "com.typesafe.akka" %% "akka-http-experimental" % VersionOf.akka,
+    "com.typesafe.akka" %% "akka-http-core"         % VersionOf.akka)
 
   lazy val testDependencies = Seq(
-                                   "org.scalatest" %% "scalatest" % "3.0.0" % "test")
+    "org.scalatest" %% "scalatest" % VersionOf.`scala-test` % "test")
 
   lazy val http4sDependencies = Seq(
-                                     "org.http4s" %% "http4s-dsl",
-                                     "org.http4s" %% "http4s-blaze-client",
-                                     "org.http4s" %% "http4s-circe"
-                                   ).map(_ % "0.14.2")
+		"org.http4s" %% "http4s-dsl",
+		"org.http4s" %% "http4s-blaze-client",
+		"org.http4s" %% "http4s-circe"
+	).map(_ % VersionOf.`http4s`)
 
   lazy val dateTimeDependencies = Seq(
-                                       "joda-time" % "joda-time" % "2.9.4")
+    "joda-time" % "joda-time" % VersionOf.`joda-time`)
 
   lazy val loggingDependencies = Seq(
-                                      "com.typesafe.scala-logging" %% "scala-logging" % "3.4.0")
+    "com.typesafe.scala-logging" %% "scala-logging" % VersionOf.`scala-logging`)
+
+  lazy val `typesafe-config` = Seq(
+    "com.typesafe" % "config" % VersionOf.`typesafe-config`)
 
   lazy val dependencies = monixDependencies ++
     circeDependencies ++
-    akkaDependencies  ++
+    `akka-http`  ++
     http4sDependencies ++
     testDependencies ++
     dateTimeDependencies
---
+++ project/VersionOf.scala
@@ -0,0 +1,10 @@
+object VersionOf {
+  val `typesafe-config` = "1.3.0"
+  val `scala-logging` = "3.4.0"
+  val `joda-time` = "2.9.4"
+  val `http4s` = "0.14.2"
+  val `scala-test` = "3.0.0"
+  val akka = "2.4.8"
+  val monix = "2.0-RC10"
+	val circe = "0.4.1"
+}
