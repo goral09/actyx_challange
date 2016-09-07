c8f4cb36380f3b8356f39254223509c5b906da34
Mateusz Gorski <mateusz.gorski@hellosoda.com>
2016-09-07 09:43:32 +0200

    Use logger instead of println.

--- server/src/main/scala/client/AlarmLogger.scala
+++ server/src/main/scala/client/AlarmLogger.scala
@@ -1,8 +1,10 @@
-package com.actyx.challenge.alarms
+package client
 
 import com.actyx.challenge.models.MachineAlarm
+import com.actyx.challenge.util.Logger
 
-trait AlarmLogger {
+trait AlarmLogger
+  extends Logger {
   def log(alarm: MachineAlarm): Unit
 }
 
@@ -12,7 +14,7 @@ object AlarmLogger {
     extends AlarmLogger {
     override def log(alarm: MachineAlarm): Unit = {
       val msg = "Machine %35s exceeded current alarm level: %2.3f / %2.3f".format(alarm.id, alarm.current, alarm.currentAlert)
-      println(msg)
+      logger.info(msg)
     }
   }
 
