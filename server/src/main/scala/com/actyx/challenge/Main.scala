a0d9fd0bae97ff42942e11a22ad2ae1534acd610
Mateusz Gorski <mateusz.gorski@hellosoda.com>
2016-09-29 10:25:07 +0200

    Factor out `web` related bits from Main.scala

--- server/src/main/scala/com/actyx/challenge/Main.scala
+++ server/src/main/scala/com/actyx/challenge/Main.scala
@@ -1,56 +1,22 @@
 package com.actyx.challenge
 
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import com.actyx.challenge.api.MachineAlarmsService
-import com.actyx.challenge.util.Logger
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Directives.{getClass ⇒ _, _}
-import akka.http.scaladsl.model.headers.HttpOriginRange
-import akka.http.scaladsl.server.Route
-import ch.megard.akka.http.cors.CorsSettings
-import client.AlarmLogger.StdOutLogger
+import akka.http.scaladsl.server.Directives.{getClass ⇒ _}
 import com.actyx.challenge.config.{Config, TypeSafeConfig}
+import com.actyx.challenge.util.Logger
+import com.actyx.challenge.web.WebMain
 import com.typesafe.config.ConfigFactory
-import monix.execution.Ack.Continue
 
 object Main extends App with Logger {
 	val typesafeConfig = ConfigFactory.load
 	implicit val config = Config()(new TypeSafeConfig(typesafeConfig))
 
-	val compositionRoot = CompositionRoot()
+	implicit val compositionRoot = CompositionRoot()
 
-	implicit val system = ActorSystem()
-	implicit val executor = system.dispatcher
-	implicit val materializer = ActorMaterializer()
+	val web = new WebMain(compositionRoot, config)
 
-	val alarmsRoute = new MachineAlarmsService(config.serverConfig,
-		                                          config.actyxParkConfig.movingAvgWindowSize)(
-                                             compositionRoot.machinesState).route
-
-	val corsSettings = CorsSettings.defaultSettings.copy(allowGenericHttpRequests = true,
-		                                                    allowedOrigins = HttpOriginRange.*)
-
-	val route =
-		get {
-			pathEndOrSingleSlash {
-				getFromResource("web/index.html")
-			} ~ path("client-fastopt.js")(getFromResource("client-fastopt.js")) ~
-				  path("client-jsdeps.js")(getFromResource("client-jsdeps.js"))
-		} ~ ch.megard.akka.http.cors.CorsDirectives.cors(corsSettings) { alarmsRoute }
-
-
-	val serverBindings = Http()
-	  .bindAndHandle(Route.handlerFlow(route),
-		                config.serverConfig.host,
-		                config.serverConfig.port)
-		.map { binding ⇒
-		  logger.info(s"Entrypoint UP on ${config.serverConfig.host}:${config.serverConfig.port}")
-	    binding
-		}
+	val closeableWeb = web.start()
 
 	sys.addShutdownHook {
-		serverBindings.foreach(_.unbind)
-		logger.info("Entrypoint DOWN")
+		closeableWeb.close()
 	}
 }
---
+++ server/src/main/scala/com/actyx/challenge/web/WebMain.scala
@@ -0,0 +1,58 @@
+package com.actyx.challenge.web
+
+import java.io.Closeable
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.headers.HttpOriginRange
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+import ch.megard.akka.http.cors.CorsSettings
+import com.actyx.challenge.CompositionRoot
+import com.actyx.challenge.Main._
+import com.actyx.challenge.api.MachineAlarmsService
+import com.actyx.challenge.config.Config
+
+class WebMain(root: CompositionRoot,
+	config:Config) {
+
+	implicit val system = ActorSystem()
+	implicit val executor = system.dispatcher
+	implicit val materializer = ActorMaterializer()
+
+
+	val alarmsRoute = new MachineAlarmsService(config.serverConfig, config.envCtxt,
+		                                          config.actyxParkConfig.movingAvgWindowSize)(
+                                               root.machinesState).route
+
+	val corsSettings = CorsSettings.defaultSettings.copy(allowGenericHttpRequests = true,
+		                                                    allowedOrigins = HttpOriginRange.*)
+
+	def start() : java.io.Closeable = {
+
+		val route =
+			get {
+				pathEndOrSingleSlash {
+					getFromResource("web/index.html")
+				} ~ path("client-fastopt.js")(getFromResource("client-fastopt.js")) ~
+					path("client-jsdeps.js")(getFromResource("client-jsdeps.js"))
+			} ~ ch.megard.akka.http.cors.CorsDirectives.cors(corsSettings) { alarmsRoute }
+
+		val serverBindings = Http()
+			.bindAndHandle(Route.handlerFlow(route),
+				              config.serverConfig.host,
+				              config.serverConfig.port)
+			.map { binding ⇒
+				logger.info(s"Entrypoint UP on ${config.serverConfig.host}:${config.serverConfig.port}")
+				binding
+			}
+
+		new Closeable {
+			override def close(): Unit = {
+				serverBindings.foreach(_.unbind)
+				logger.info("Entrypoint DOWN")
+			}
+		}
+	}
+}
